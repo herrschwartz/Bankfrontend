faf157a3386cfcba05f2aa6258c0dd4e88377299
Tim Schwartz <herrschwartz@gmail.com>
2019-11-06 21:25:27 -0800

    initial2

--- main.lua
+++ main.lua
@@ -1,4 +1,9 @@
-local texture, itemCount, locked, quality, readable = GetContainerItemInfo(1,2);
-DEFAULT_CHAT_FRAME:AddMessage(itemCount .. " stacked items in 2nd slot of bag 1.");
+-- local texture, itemCount, locked, quality, readable = GetContainerItemInfo(1,2);
+-- DEFAULT_CHAT_FRAME:AddMessage(itemCount .. " stacked items in 2nd slot of bag 1.");
+DEFAULT_CHAT_FRAME:AddMessage( (select(7, GetContainerItemInfo(1,1))) );
+DEFAULT_CHAT_FRAME:AddMessage( GetContainerItemID(1, 1));
+message("this addon blows");
+message("things" .. (select(7, GetContainerItemInfo(1,1))));
+message("stuff" .. GetContainerItemID(1, 1));
 -- itemId = GetContainerItemID(0, 1);
 -- DEFAULT_CHAT_FRAME:AddMessage(itemId);
